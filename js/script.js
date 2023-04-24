a14945537fc15d5dac26026bd8ebf53114c07eef
Vitu Vitu <fv.victor97@gmail.com>
2023-04-24 20:59:51 -0300

    pokedex

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonID = document.querySelector('.pokemon__id');
+const pokemonImage = document.querySelector('.pokemon__image');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const ButtonPrev = document.querySelector('.prev');
+const ButtonNext = document.querySelector('.next');
+const ButtonSearch = document.querySelector('.ok');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading...';
+    pokemonID.innerHTML = '';
+
+    const data = await fetchPokemon(pokemon);
+
+    if (data) {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonID.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Not Found';
+        pokemonID.innerHTML = '00';
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
+
+ButtonPrev.addEventListener('click', () => {
+    if(searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon)
+    };
+});
+
+ButtonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+ButtonSearch.addEventListener('click', () => {
+    const ok = input.value;
+});
+
+renderPokemon(searchPokemon);
